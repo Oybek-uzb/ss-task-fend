09aa54c4b2dec8e393618d9de00b7e909dd12544
Oybek-uzb <78894838+Oybek-uzb@users.noreply.github.com>
2022-06-06 01:21:01 +0500

    minio added

--- chat-socket.js
+++ chat-socket.js
@@ -1,7 +1,8 @@
-const socket = io("http://localhost:3000");
+const socket = io("http://localhost:80");
 
 const message = document.getElementById("message");
 const messages = document.getElementById("messages");
+const file = document.getElementById("file");
 
 const handleSubmitNewMessage = () => {
     socket.emit("message", {
@@ -32,4 +33,25 @@ const buildNewMessage = (message) => {
     div.style.display = "flex"
     div.style.wordBreak = "break-all"
     return div;
+}
+
+const handleSendFile = async () => {
+    const formData = new FormData()
+    formData.set("file", file.files[0])
+
+    const res = await fetch("http://localhost:3000", {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'omit',
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: formData
+    })
+
+    console.log(formData.get("file"))
+    console.log(await res)
 }
