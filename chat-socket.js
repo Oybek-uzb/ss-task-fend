2694eb3782488b2d5fdb0c0c470f5539df1a3626
Oybek-uzb <78894838+Oybek-uzb@users.noreply.github.com>
2022-06-05 07:51:05 +0500

    sides detected

--- chat-socket.js
+++ chat-socket.js
@@ -4,20 +4,33 @@ const message = document.getElementById("message");
 const messages = document.getElementById("messages");
 
 const handleSubmitNewMessage = () => {
-    socket.emit("message", { data: message.value });
+    socket.emit("message", {
+        socketId: socket.id,
+        message: message.value
+    });
 }
 
-socket.on("message", ({ data }) => {
-    handleNewMessage(data);
+socket.on("message", ({ socketId, message }) => {
+    handleNewMessage(socketId, message);
 })
 
-const handleNewMessage = (message) => {
-    messages.appendChild(buildNewMessage(message));
+const handleNewMessage = (socketId, message) => {
+    const newMessage = buildNewMessage(message)
+
+    if (socket.id === socketId) {
+        newMessage.style.justifyContent = "flex-end"
+        newMessage.style.padding = "5px 10px 5px 40px"
+    }
+
+    messages.appendChild(newMessage);
 }
 
 const buildNewMessage = (message) => {
     const div = document.createElement("div");
     div.appendChild(document.createTextNode(message));
-    div.style.margin = "8px"
+    div.style.padding = "5px 40px 5px 10px"
+    div.style.display = "flex"
+    // div.style.margin = "px"
+    div.style.wordBreak = "break-all"
     return div;
 }
